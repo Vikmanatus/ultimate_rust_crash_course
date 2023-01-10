1fed7f22ff8b24a9182ae089b8eaeb2dc25e8739
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-01-10 15:29:24 +0100

    done first exercise

---
+++ tutorials/variables/src/main.rs
@@ -0,0 +1,35 @@
+// const STARTING_MISSILES:i32 = 8;
+// const READY_AMOUNT:i32 = 2;
+
+fn main() {
+    // Exercise 1
+    // let mut missiles = 8;
+    // let ready = 2;
+    // println!("Firing {} of my {} missiles...", ready, missiles);
+
+    // missiles = missiles - ready;
+    // println!("{} missiles left", missiles);
+
+    // Exercise 2
+    // let mut missiles = STARTING_MISSILES;
+    // let ready = READY_AMOUNT;
+
+    // println!("Firing {} of my {} missiles...", ready, missiles);
+    // missiles = missiles - ready;
+    // println!("{} missiles left", missiles);
+
+    // Extra challenges Part 1
+    // let mut missiles:i32 = STARTING_MISSILES;
+    // let ready:i32 = READY_AMOUNT;
+
+    // println!("Firing {} of my {} missiles...", ready, missiles);
+    // missiles = missiles - ready;
+    // println!("{} missiles left", missiles);
+
+    // Extra challenges Part 2
+    // let (mut missiles,ready) = (STARTING_MISSILES, READY_AMOUNT);
+    // println!("Firing {} of my {} missiles...", ready, missiles);
+    // missiles = missiles - ready;
+    // println!("{} missiles left", missiles);
+    println!("Exercise done");
+}
